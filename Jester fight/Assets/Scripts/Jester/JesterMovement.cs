8ba36a3986f6536e658496fe671c736400f4107a
EmirhanFaruk <emirhansagir74@gmail.com>
2024-01-27 13:19:34 +0100

    Improving movement

--- Jester fight/Assets/Scripts/Jester/JesterMovement.cs
+++ Jester fight/Assets/Scripts/Jester/JesterMovement.cs
@@ -6,18 +6,76 @@ public class JesterMovement : MonoBehaviour
 {
     private CharacterController characterController;
     private Vector2 jesterVel;
-    private float speed;
+    public float speed = 5f;
+    public float jumpHeight = 1000f;
+
+    private string jName; // either Jester1 or Jester2
+
+    private bool onGround;
+    private float gravity = 9.8f;
+    private Ray ray;
+    private RaycastHit raycastHit;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
-        speed = 5f;
+        onGround = true;
+        jName = gameObject.name;
+        ray = new Ray(transform.position, -transform.up);
     }
 
+
+    // Move in the horizontal axis
+    private void MoveH()
+    {
+        float movH = Input.GetAxis(jName + "H");
+        jesterVel.x = movH * speed * Time.deltaTime;
+    }
+
+    // Jump
+    private void Jump() 
+    { 
+        if(onGround)
+        {
+            if (Input.GetButton(jName + "J"))
+            {
+                onGround = false;
+                jesterVel.y += jumpHeight * Time.deltaTime;
+            }
+        }
+    }
+
+    
+    private void ApplyGravity()
+    {
+        if (!onGround)
+        {
+            jesterVel.y -= gravity * Time.deltaTime;
+        }
+        else
+        {
+            jesterVel.y = 0;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (Physics.Raycast(ray, out raycastHit))
+        {
+            if (raycastHit.collider.gameObject.name == "Platform")
+            {
+                onGround = true;
+            }
+        }
+    }
+
+
+    // Makes the jester move
     public void Update()
     {
-        float movH = Input.GetAxis("Jester1H");
-        jesterVel.x = movH * speed * Time.deltaTime;
+        ApplyGravity();
+        MoveH();
+        Jump();
         characterController.Move(jesterVel);
     }
 }
