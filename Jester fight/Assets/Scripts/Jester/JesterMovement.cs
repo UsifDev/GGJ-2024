cf0addf42af75e19dfc9e846582724e2ddb604f9
EmirhanFaruk <emirhansagir74@gmail.com>
2024-01-27 16:38:29 +0100

    Horizontal movement done

--- Jester fight/Assets/Scripts/Jester/JesterMovement.cs
+++ Jester fight/Assets/Scripts/Jester/JesterMovement.cs
@@ -6,19 +6,16 @@ public class JesterMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 jesterVel;
-    public float speed = 5;
-    public float maxSpeed = 1000f;
-    public float jumpHeight = 100f;
+    public float speed = 5f;
+    public float maxHorSpeed = 5f;
 
     private string jName; // either Jester1 or Jester2
 
-    private bool onGround;
     public LayerMask groundLayer;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        onGround = true;
         jName = gameObject.name;
     }
 
@@ -30,43 +27,27 @@ public class JesterMovement : MonoBehaviour
         jesterVel.x = movH * speed * Time.deltaTime;
     }
 
-    /*
 
-    // Jump
-    private void Jump() 
-    { 
-        if(onGround)
-        {
-            if (Input.GetButton(jName + "J"))
-            {
-                onGround = false;
-                jesterVel.y += jumpHeight * Time.deltaTime;
-            }
-        }
-    }
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private Vector2 Limit(Vector2 v)
     {
-        if (collision.gameObject.tag == "Platform")
+        float x = v.x;
+        if(v.x > maxHorSpeed)
         {
-            onGround = true;
+            x = maxHorSpeed;
         }
-        else
+        else if (v.x < -maxHorSpeed)
         {
-            onGround = false;
+            x = -maxHorSpeed;
         }
-    }
-    */
 
+        return new Vector2 (x, v.y);
+    }
 
     // Makes the jester move
     public void Update()
     {
         MoveH();
-        // Jump();
-        rb.velocity = jesterVel;
-        // rb.velocity = Limit(rb.velocity);
-        Debug.Log(onGround + ", " + rb.velocity);
+        rb.velocity += jesterVel;
+        rb.velocity = Limit(rb.velocity);
     }
 }
