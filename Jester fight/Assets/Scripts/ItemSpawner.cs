122d627a8bb936e98585f983de5f9d75816937e6
YousifAljbouri <154931090+UsifDev@users.noreply.github.com>
2024-01-27 22:10:49 +0300

    added spawner

---
+++ Jester fight/Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,49 @@
+
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject bananaPeelItem;
+    public GameObject rakeItem;
+    public GameObject bombItem;
+    public GameObject ballItem;
+
+    public float spawnY;
+    public float leftMaximumPosition;
+    public float RightMaximumPosition;
+
+    private GameObject[] items;
+
+    void Start()
+    {
+        items = new GameObject[4];
+        items[0] = bananaPeelItem;
+        items[1] = rakeItem;
+        items[2] = bombItem;
+        items[3] = ballItem;
+
+        InvokeRepeating("SpawnRandomItem", 2.0f, 3.0f);
+    }
+
+    void Update()
+    {
+        // if(Input.anyKeyDown)
+        // {
+        //     CancelInvoke("SpawnRandomItem");
+        // }
+    }
+
+    //user - defined functions
+    private void SpawnItem(GameObject prefab, Vector3 position)
+    {
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+
+    private void SpawnRandomItem()
+    {
+        Vector3 position = new Vector3(Random.Range(leftMaximumPosition, RightMaximumPosition) ,spawnY ,0);
+        int itemIndex = Mathf.RoundToInt(Random.Range(0,4));
+        SpawnItem(items[itemIndex], position);
+    }
+
+}
--- Jester fight/Assets/Scripts/ScoreTracker.cs
+++ Jester fight/Assets/Scripts/ScoreTracker.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class scr : MonoBehaviour
+public class ScoreTracker: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
